e4a142c3d56ce8cdc65b725bef8ca26ef236239e
Lindsay Preston <lindsayarthur303@gmail.com>
2019-08-25 16:35:22 -0600

    click

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => (
+  <footer className="footer text-center">
+    <span>Clicky Game &nbsp;<a href="https://github.com/Lindsay303" target="_blank" rel="noopener noreferrer">Lindsay Preston</a></span>
+  </footer>
+);
+
+export default Footer;
